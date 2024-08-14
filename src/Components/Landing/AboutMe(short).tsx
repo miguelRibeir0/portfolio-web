9b4c9317c6e825e8336b7a0815cb5a80302350cd
Miguel <miguelsribeiro7@gmail.com>
2024-08-14 18:04:30 +0100

    ...

--- src/Components/Landing/AboutMe(short).tsx
+++ src/Components/Landing/AboutMe(short).tsx
@@ -15,7 +15,10 @@ const ShortAbout = () => {
       <div className="lg:w-1/2 w-full">
         <Fade>
           {inView && (
-            <Typewriter text="Hi! I'm a full-stack developer from Portugal with a passion for the world of web development and all of it's capabilities. I have experience working with a variety of technologies and am always looking to learn more. I'm currently looking for new opportunities to grow and develop my skills." />
+            <Typewriter
+              text="Hi! I'm a full-stack developer from Portugal with a passion for the world of web development and all of it's capabilities. I have experience working with a variety of technologies and am always looking to learn more. I'm currently looking for new opportunities to grow and develop my skills."
+              speed={10}
+            />
           )}
         </Fade>
       </div>
